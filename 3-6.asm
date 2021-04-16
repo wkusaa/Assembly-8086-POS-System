227f7b74d022aea4297114474f80df1b7baede85
pikachu830 <songkai135790@gmail.com>
2021-04-16 19:01:42 +0800

    commit by sk

--- 3-6.asm
+++ 3-6.asm
@@ -528,24 +528,6 @@ ADMIN:
         MOV AH,09H
         LEA DX,NL
         INT 21H
-
-        MOV AH,09H
-        LEA DX,STR3
-        INT 21H
-
-        MOV AH,01H
-        INT 21H
-
-        CMP AL,'y'
-        JE ORDERING2
-        JMP CALCULATE
-ORDERING2:
-        JMP ORDERING
-CALCULATE:
-        MOV AH,09H
-        LEA DX,NL
-        INT 21H
-
         MOV AH,09H;DISPLAY SUBTOTAL
         LEA DX,STRSUBTOTAL
         INT 21H
@@ -555,7 +537,7 @@ CALCULATE:
         INT 21H
 
         MOV AX,SUBTOTAL
-        MOV TOTAL,AX
+        ADD TOTAL,AX
 
         DIV HUNDRED
         MOV REMAINDER,AH
@@ -588,6 +570,23 @@ CALCULATE:
         ADD DL,30H
         INT 21H
 
+        MOV AH,09H
+        LEA DX,NL
+        INT 21H
+
+        MOV AH,09H
+        LEA DX,STR3
+        INT 21H
+
+        MOV AH,01H
+        INT 21H
+
+        CMP AL,'y'
+        JE ORDERING2
+        JMP CALCULATE
+ORDERING2:
+        JMP ORDERING
+CALCULATE:
         MOV AH,09H
         LEA DX,NL
         INT 21H
@@ -817,23 +816,6 @@ ADMIN1:
         LEA DX,NL
         INT 21H
 
-        MOV AH,09H
-        LEA DX,STR3
-        INT 21H
-
-        MOV AH,01H
-        INT 21H
-
-        CMP AL,'y'
-        JE ORDERINGT
-        JMP CALCULATE1
-ORDERINGT:
-        JMP ORDERING
-CALCULATE1:
-        MOV AH,09H
-        LEA DX,NL
-        INT 21H
-
         MOV AH,09H;DISPLAY SUBTOTAL
         LEA DX,STRSUBTOTAL
         INT 21H
@@ -843,7 +825,7 @@ CALCULATE1:
         INT 21H
 
         MOV AX,SUBTOTAL
-        MOV TOTAL,AX
+        ADD TOTAL,AX
 
         DIV HUNDRED
         MOV REMAINDER,AH
@@ -876,6 +858,24 @@ CALCULATE1:
         ADD DL,30H
         INT 21H
 
+        MOV AH,09H
+        LEA DX,NL
+        INT 21H
+
+        MOV AH,09H
+        LEA DX,STR3
+        INT 21H
+
+        MOV AH,01H
+        INT 21H
+
+        CMP AL,'y'
+        JE ORDERINGT
+        JMP CALCULATE1
+ORDERINGT:
+        JMP ORDERING
+CALCULATE1:
+       
         MOV AH,09H
         LEA DX,NL
         INT 21H
@@ -1104,23 +1104,6 @@ ADMIN2:
         LEA DX,NL
         INT 21H
 
-        MOV AH,09H
-        LEA DX,STR3
-        INT 21H
-
-        MOV AH,01H
-        INT 21H
-
-        CMP AL,'y'
-        JE ORDERINGZ
-        JMP CALCULATE2
-ORDERINGZ:
-        JMP ORDERING
-CALCULATE2:
-        MOV AH,09H
-        LEA DX,NL
-        INT 21H
-
         MOV AH,09H;DISPLAY SUBTOTAL
         LEA DX,STRSUBTOTAL
         INT 21H
@@ -1130,7 +1113,7 @@ CALCULATE2:
         INT 21H
 
         MOV AX,SUBTOTAL
-        MOV TOTAL,AX
+        ADD TOTAL,AX
 
         DIV HUNDRED
         MOV REMAINDER,AH
@@ -1162,6 +1145,24 @@ CALCULATE2:
         MOV DL,BH
         ADD DL,30H
         INT 21H
+
+        MOV AH,09H
+        LEA DX,NL
+        INT 21H
+
+        MOV AH,09H
+        LEA DX,STR3
+        INT 21H
+
+        MOV AH,01H
+        INT 21H
+
+        CMP AL,'y'
+        JE ORDERINGZ
+        JMP CALCULATE2
+ORDERINGZ:
+        JMP ORDERING
+CALCULATE2:
 
         MOV AH,09H
         LEA DX,NL
@@ -1387,27 +1388,6 @@ ADMIN4:
         MUL QTT
         MOV SUBTOTAL,AX
 
-        MOV AH,09H
-        LEA DX,NL
-        INT 21H
-
-        MOV AH,09H
-        LEA DX,STR3
-        INT 21H
-
-        MOV AH,01H
-        INT 21H
-
-        CMP AL,'y'
-        JE ORDERING6
-        JMP CALCULATE3
-ORDERING6:
-        JMP ORDERING
-CALCULATE3:
-        MOV AH,09H
-        LEA DX,NL
-        INT 21H
-
         MOV AH,09H;DISPLAY SUBTOTAL
         LEA DX,STRSUBTOTAL
         INT 21H
@@ -1417,7 +1397,7 @@ CALCULATE3:
         INT 21H
 
         MOV AX,SUBTOTAL
-        MOV TOTAL,AX
+        ADD TOTAL,AX
 
         DIV HUNDRED
         MOV REMAINDER,AH
@@ -1449,6 +1429,28 @@ CALCULATE3:
         MOV DL,BH
         ADD DL,30H
         INT 21H
+
+        MOV AH,09H
+        LEA DX,NL
+        INT 21H
+
+        MOV AH,09H
+        LEA DX,NL
+        INT 21H
+
+        MOV AH,09H
+        LEA DX,STR3
+        INT 21H
+
+        MOV AH,01H
+        INT 21H
+
+        CMP AL,'y'
+        JE ORDERING6
+        JMP CALCULATE3
+ORDERING6:
+        JMP ORDERING
+CALCULATE3:
 
         MOV AH,09H
         LEA DX,NL
